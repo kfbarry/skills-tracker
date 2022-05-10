d40e3e0f416ab51796fa4167677d558078e86194
Kieran <kieranbarry51@gmail.com>
2022-05-09 20:11:23 -0500

    finished studio

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -2,6 +2,7 @@ package org.launchcode.skillstracker.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -23,17 +24,52 @@ public class SkillsController {
                 "</html>";
     }
 
+    @GetMapping("entries")
+    @ResponseBody
+    public String createSkillsTrackerMessage(@RequestParam String name, @RequestParam String language1, @RequestParam String language2, @RequestParam String language3){
+        return "<html>" +
+                "<body>" +
+                "<h1>" + name + "</h1>" +
+                "<ol>" +
+                "<li>" + language1 + "</li>" +
+                "<li>" + language2 + "</li>" +
+                "<li>" + language3 + "</li>" +
+                "</ol>" +
+                "</body>" +
+                "</html>";
+    }
+
     @GetMapping("form")
+    @ResponseBody
     public String rankOptions(){
         return "<html>" +
                 "<body>" +
                 "<form action='entries'>" +
-                "<ol>" +
-                "<li>Name</li>" +
-                "<li><input type='text' name='name'></li>" +
-                "<li>My Favorite Language</li>" +
-                "<li><input " +
-                "</ol>" +
+                "<h1>Name</h1>" +
+                "<input type='text' name='name'>" +
+                "<h2>My Favorite Language</h2>" +
+                "<select name='language1' id='language-select>" +
+                "<option value='selectValue>Select Language</option>" +
+                "<option value='javascript'>JavaScript</option>" +
+                "<option value='java'>Java</option>" +
+                "<option value='sql'>SQL</option>" +
+                "</select>" +
+                "<h2>My Second Favorite Language</h2>" +
+                "<select name='language2' id='language-select>" +
+                "<option value='selectValue>Select Language</option>" +
+                "<option value='javascript'>JavaScript</option>" +
+                "<option value='java'>Java</option>" +
+                "<option value='sql'>SQL</option>" +
+                "</select>" +
+                "<h2>My Third Favorite Language</h2>" +
+                "<select name='language3' id='language-select>" +
+                "<option value='selectValue>Select Language</option>" +
+                "<option value='javascript'>JavaScript</option>" +
+                "<option value='java'>Java</option>" +
+                "<option value='sql'>SQL</option>" +
+                "</select>" +
+                "<input type='submit' value='Submit!'>" +
+                "</form>" +
                 "</body>" +
                 "</html>";
     }
